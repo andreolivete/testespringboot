bd95175c3ca9dcffcbe2197ce5ba4d83c9bcf29e
andreolivete <andreolivete@gmail.com>
2018-04-20 19:35:26 -0300

    Projeto Inicial

---
+++ src/main/java/com/alo/tsb/TestespringbootApplication.java
@@ -0,0 +1,12 @@
+package com.alo.tsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestespringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestespringbootApplication.class, args);
+	}
+}
